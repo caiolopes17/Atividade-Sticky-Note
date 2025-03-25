55de452acd4d56efdc17337f96af9f195841468d
CAIO LOPES DURAES <caio.lduraes@senacsp.edu.br>
2025-03-25 19:18:15 -0300

    atualizar

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 console.log("Electron - Processo principal")
 
-// Importação dos recursos do framework
-// App se refere a (Aplicação)
-// BrowserWindow (Criação da Janela)
-//nativeTheme esta relacionado para o usuário escolher o tema claro ou escuro nas configurações do windows
-// Menu (Definir um menu personalizado)
-// Shell (Acessar links externos no navegador padrão (Ex: GitHub))
-const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
+// importação dos recursos do framework
+// app (aplicação)
+// BrowserWindow (criação da janela)
+// nativeTheme (definir tema claro ou escuro)
+// Menu (definir um menu personalizado)
+// shell (acessar links externos no navegador padrão)
+// ipcMain (permite estabelecer uma comunicação entre processos (IPC) main.js <=> renderer.js)
+const { app, BrowserWindow, nativeTheme, Menu, shell, ipcMain } = require('electron/main')
 
-// Janela Principal
+// Ativação do preload.js (importação do path)
+const path = require('node:path')
+
+// Importação dos métodos conectar e desconectar (módulo de conexão)
+const { conectar, desconectar } = require('./database.js')
+
+// Janela principal
 let win
 const createWindow = () => {
-  // Definindo o tema da janelaclaro ou escuro
-  nativeTheme.themeSource = 'dark'
+  // definindo o tema da janela claro ou ecuro
+  nativeTheme.themeSource = 'light'
   win = new BrowserWindow({
-    width: 800,
+    width: 1010,
     height: 720,
-    //frame: false
-    resizable: false,
+    //frame: false,
+    //resizable: false,
     //minimizable: false,
     //closable: false,
-    //autoHideMenuBar: true
+    //autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   // Carregar o menu personalizado
@@ -31,33 +41,88 @@ const createWindow = () => {
   win.loadFile('./src/views/index.html')
 }
 
-// Janela Sobre
+// janela sobre
+let about
 function aboutWindow() {
   nativeTheme.themeSource = 'light'
-  // Obter a janela principal, comando abaixo sabera qual é a janela principal
+  // obter a janela principal
   const mainWindow = BrowserWindow.getFocusedWindow()
-  // Validação (Se existir a janela principal)
+  // validação (se existir a janela principal)
   if (mainWindow) {
     about = new BrowserWindow({
-      width: 320,
-      height: 280,
+      width: 300,
+      height: 200,
       autoHideMenuBar: true,
       resizable: false,
       minimizable: false,
-      // Estabelecer uma relação hierárquica entre janelas
+      // estabelecer uma relação hierárquica entre janelas
       parent: mainWindow,
-      // Criar uma janela modal (só retorna a principal quando encerrada)
-      modal: true
+      // criar uma janela modal (só retorna a principal quando encerrada)
+      modal: true,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
     })
   }
 
   about.loadFile('./src/views/sobre.html')
+
+  //recebimento da mensagem do renderizador da tela sobre para fechar a janela usando o botão OK
+  ipcMain.on('about-exit', () => {
+    //validação (se existir a janela e ela não estiver sido destruída, fechar)
+    if (about && !about.isDestroyed()) {
+      about.close()
+    }   
+  })
 }
 
-// inicialização da aplicação ( ".then" significa assincronismo)
+// janela nota
+let note
+function noteWindow() {
+  nativeTheme.themeSource = 'light'
+  // obter a janela principal
+  const mainWindow = BrowserWindow.getFocusedWindow()
+  // validação (se existir a janela principal)
+  if (mainWindow) {
+    note = new BrowserWindow({
+      width: 400,
+      height: 270,
+      autoHideMenuBar: true,
+      resizable: false,
+      minimizable: false,
+      // estabelecer uma relação hierárquica entre janelas
+      parent: mainWindow,
+      // criar uma janela modal (só retorna a principal quando encerrada)
+      modal: true,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  }
 
+  note.loadFile('./src/views/nota.html')  
+}
+
+// inicialização da aplicação (assíncronismo)
 app.whenReady().then(() => {
   createWindow()
+
+  // Melhor local para estabelecer a conexão com o banco de dados
+  // No MongoDB é mais eficiente manter uma única conexão aberta durante todo o tempo de vida do aplicativo e encerrar a conexão quando o aplicativo for finalizado
+  // ipcMain.on (receber mensagem)
+  // db-connect (rótulo da mensagem)
+  ipcMain.on('db-connect', async (event) => {
+    //a linha abaixo estabelece a conexão com o banco de dados e verifica se foi conectado com sucesso (return true)
+    const conectado = await conectar()
+    if (conectado) {
+      // enviar ao renderizador uma mensagem para trocar a imagem do ícone do status do banco de dados (criar um delay de 0.5 ou 1s para sincronização com a nuvem)
+      setTimeout(() => {
+        // enviar ao renderizador a mensagem "conectado"
+        // db-status (IPC - comunicação entre processos - preload.js)
+        event.reply('db-status', "conectado")
+      }, 500) //500ms = 0.5s
+    }
+  })
 
   // só ativar a janela principal se nenhuma outra estiver ativa
   app.on('activate', () => {
@@ -67,25 +132,30 @@ app.whenReady().then(() => {
   })
 })
 
-// se o sistema não for MAC encerrar a aplicação quando a janela for fechada
+// se o sistem não for MAC encerrar a aplicação quando a janela for fechada
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
+// IMPORTANTE! Desconectar do banco de dados quando a aplicação for finalizada
+app.on('before-quit', async () => {
+  await desconectar()
+})
+
 // Reduzir a verbosidade de logs não críticos (devtools)
 app.commandLine.appendSwitch('log-level', '3')
 
-// Template do menu
-// Quando abre e fecha essas chaves [] significa vetor, Label (Escrever no menu), Ctrl+N (Tecla atalho), app.quit (Finaliza o aplicativo)
+// template do menu
 const template = [
   {
-    label: 'Cadastro',
+    label: 'Notas',
     submenu: [
       {
-        label: 'Criar cadastro',
+        label: 'Criar nota',
         accelerator: 'Ctrl+N',
+        click: () => noteWindow()
       },
       {
         type: 'separator'
@@ -116,6 +186,10 @@ const template = [
         type: 'separator'
       },
       {
+        label: 'Recarregar',
+        role: 'reload'
+      },
+      {
         label: 'DevTools',
         role: 'toggleDevTools'
       }
@@ -126,7 +200,7 @@ const template = [
     submenu: [
       {
         label: 'Repositório',
-        click: () => shell.openExternal('https://github.com/caiolopes17/Atividade-Sticky-Not')
+        click: () => shell.openExternal('alterar')
       },
       {
         label: 'Sobre',
